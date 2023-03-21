4c7a4d6d8e3a1b24d4ef8e0baf4f9eb45583ffea
etovara <edwintovaraladejo@gmail.com>
2023-03-21 11:29:00 -0300

    Mejora

--- POMPageObjetModel/Pages/loginPage.py
+++ POMPageObjetModel/Pages/loginPage.py
@@ -1,20 +1,21 @@
+from selenium.webdriver.common.by import By
 # Se debe crear una clase
 class loginPage():
-  
+
     # Se debe crear un contructor +  un argumento para el controlador 
     def __init__(self, driver):
       self.driver = driver
     
     # Se debe agregar los objectos existente en la pagina
-      self.singUp= "signin2"
-      self.username_textbox_id = "sign-username"
-      self.password_textbox_id = "sign-password"
-      self.cancelar_XPATH = "//*[@id="signInModal"]/div/div/div[3]/button[1]"
+      self.register_button_id       = 'signin2'
+      self.username_textbox_id      = "sign-username"
+      self.password_textbox_id      = "sign-password"
+      self.click_cancelar_xpath           = '//*[@id="signInModal"]/div/div/div[3]/button[1]'
       
       
-    # Se debe crear una función o metodo de acción
+    # Se debe crear una función o metodo de acción 
     def click_singUp(self):
-      self.driver.find_element(By.ID, self.singUp).click()
+      self.driver.find_element(By.ID, self.register_button_id).click()
       
     
     def enter_username(self, username):
@@ -25,11 +26,11 @@ class loginPage():
     def enter_password(self, password):
       self.driver.find_element(By.ID, self.password_textbox_id).clear()
       self.driver.find_element(By.ID, self.password_textbox_id).send_keys(password)
-      
+            
       
     def click_cancelar(self):
-      self.driver.find_element(By.XPATH, self.cancelar_XPATH).click()
-      
+      self.driver.find_element(By.XPATH, self.click_cancelar_xpath).click()
+     
 
 
 #Una vez que se declaran la funciones / metodos en los Pages se debe ir a los test generados para comentar o se pueden eliminar de acuerdo a las politicas de empresa.
--- POMPageObjetModel/Test/login.py
+++ POMPageObjetModel/Test/login.py
@@ -14,28 +14,29 @@ class RegistroTest(unittest.TestCase):
 # Se debe declarar las funciones de configuración setUpClass(cls) antes de que se ejecuten las pruebas en una clase individual.
 
   @classmethod
-  def setUpClass(cls):
+  def setUpClass(cls): # Un método de clase llamado antes de que los tests en una clase individual sean ejecutados
+    
     #Ubicacion del Driver y Url prueba
     cls.driver = webdriver.Chrome(executable_path= r"C:\Users\Edwin Tovar\Desktop\proyectoPom\chromedriver\chromedriver.exe")
+    cls.driver.implicitly_wait(10)
     cls.driver.maximize_window()
   
   # Se va a estar creando un nuevo objeto el cual va a estar realizando el llamado a los Page de las clases, esto es para poder tener un controlador de recorrido declarado y no automatico.
   
   def test_login_valid(self):
     driver = self.driver
-    
     driver.get("https://www.demoblaze.com/index.html")
     time.sleep(2)
     
-    primera = loginPage(driver)
+    login = loginPage(driver)
     time.sleep(2)
-    loginPage.click_singUp()
+    login.click_singUp()
     time.sleep(2)
-    loginPage.enter_username("EDTOVAR")
+    login.enter_username("EDTOVAR")
     time.sleep(2)
-    loginPage.login.enter_password("12345")
+    login.enter_password("12345")
     time.sleep(2)
-    loginPage.click_cancelar()
+    login.click_cancelar()
     
       
   
@@ -64,12 +65,11 @@ class RegistroTest(unittest.TestCase):
    """
    
   @classmethod  
-  def tearDownClass(cls):
-    #Cerrar Navegador
+  def tearDownClass(cls): # Un método de clase llamado después de que se hayan realizado tests en una clase individual.
     cls.driver.close()
     cls.driver.quit()
     time.sleep(3)
     print("Test Completado")
 
-if __name__=="__main__":
+if __name__ == '__main__':
     unittest.main()
